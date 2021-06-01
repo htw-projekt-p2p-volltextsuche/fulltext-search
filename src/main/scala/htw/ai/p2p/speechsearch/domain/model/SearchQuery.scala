182397ae1383510724dd829b9ab207569bb2a994
Joscha Seelig <s0566845@htw-berlin.de>
2021-06-01 23:24:40 +0200

    rename api param and add domain model for SearchQuery

---
+++ src/main/scala/htw/ai/p2p/speechsearch/domain/model/SearchQuery.scala
@@ -0,0 +1,27 @@
+package htw.ai.p2p.speechsearch.domain.model
+
+/**
+  * @author Joscha Seelig <jduesentrieb> 2021
+**/
+case class SearchQuery(
+    maxResults: Int,
+    target: String,
+    terms: String,
+    extensions: List[QueryExtension]
+)
+
+case class QueryExtension(
+    connector: QueryConnector,
+    target: String,
+    terms: String
+)
+
+sealed trait QueryTarget
+case object FullText extends QueryTarget
+case object Affiliation extends QueryTarget
+case object Speaker extends QueryTarget
+
+sealed trait QueryConnector
+case object Or extends QueryConnector
+case object And extends QueryConnector
+case object AndNot extends QueryConnector
