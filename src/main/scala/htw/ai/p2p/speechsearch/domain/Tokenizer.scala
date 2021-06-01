eb2059e4f106b45533cf1564f95b029c0c80949d
Joscha Seelig <s0566845@htw-berlin.de>
2021-06-01 23:14:41 +0200

    update api spec in consultation with web-ui dev

--- src/main/scala/htw/ai/p2p/speechsearch/domain/Tokenizer.scala
+++ src/main/scala/htw/ai/p2p/speechsearch/domain/Tokenizer.scala
@@ -1,34 +1,31 @@
 package htw.ai.p2p.speechsearch.domain
 
+import htw.ai.p2p.speechsearch.domain.Tokenizer.StopWords
+
 import scala.io.Source.fromResource
-import scala.util.{Failure, Success, Using}
+import scala.util.Using
 
 /**
   * @author Joscha Seelig <jduesentrieb> 2021
  **/
 object Tokenizer {
+
   def apply(): Tokenizer = new Tokenizer()
+
+  private val StopWords =
+    Using(fromResource("stopwords_de.txt"))(_.getLines.toList)
+      .fold(e => throw new IllegalStateException(e), identity)
 }
 
 class Tokenizer {
 
   private val DelimiterPattern = """[\s,.?!:'"]"""
 
-  private val stopWords = readStopWords
-
   def apply(text: String): List[String] =
     text.toLowerCase
       .split(DelimiterPattern)
-      .filterNot(stopWords.contains)
-      .filter(!_.isBlank)
+      .view
+      .filterNot(StopWords.contains)
+      .filterNot(_.isBlank)
       .toList
-
-  private def readStopWords: List[String] =
-    Using(fromResource("stopwords_de.txt"))(_.getLines.toList)
-//     .fold(throw new IllegalStateException) ()
-    match {
-      case Success(content) => content
-      case Failure(e)       => throw new IllegalStateException(e)
-    }
-
 }
