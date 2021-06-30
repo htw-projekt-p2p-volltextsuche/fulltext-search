05b41ad2c94af87bae5ca1cae80ca822b07e4a6d
zink-l <leon.enzenberger@protonmail.com>
2021-06-30 17:08:34 +0200

    prepare main branch for deployment

--- src/main/scala/htw/ai/p2p/speechsearch/SpeechSearchServer.scala
+++ src/main/scala/htw/ai/p2p/speechsearch/SpeechSearchServer.scala
@@ -1,42 +1,52 @@
 package htw.ai.p2p.speechsearch
 
-import cats.effect.{ConcurrentEffect, ExitCode, _}
-import cats.implicits.toSemigroupKOps
+import cats.effect._
+import cats.implicits._
 import fs2.Stream
-import htw.ai.p2p.speechsearch.api.index.{IndexRoutes, IndexService}
-import htw.ai.p2p.speechsearch.api.searches.{SearchRoutes, SearchService}
+import htw.ai.p2p.speechsearch.api.index._
+import htw.ai.p2p.speechsearch.api.searches._
 import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.middleware.Logger
+import org.http4s.server.middleware._
 
 import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration.DurationInt
 
 /**
  * @author Joscha Seelig <jduesentrieb> 2021
  */
 object SpeechSearchServer {
 
+  private val corsConfig = CORSConfig(
+    anyOrigin = true,
+    allowCredentials = true,
+    maxAge = 1.day.toSeconds,
+    anyMethod = false,
+    allowedMethods = Set("POST").some
+  )
+
   def stream[F[_]: ConcurrentEffect](
     port: Int,
-    searchAlg: SearchService[F],
-    indexAlg: IndexService[F],
+    searchService: SearchService[F],
+    indexService: IndexService[F],
     apiPrefix: String
   )(implicit T: Timer[F]): Stream[F, ExitCode] = {
-    val httpApp = Router(
+    val httpService = Router(
       apiPrefix -> (
-        new SearchRoutes[F](searchAlg).routes <+> new IndexRoutes[F](
-          indexAlg
-        ).routes
+        new SearchRoutes[F](searchService).routes
+          <+> new IndexRoutes[F](indexService).routes
       )
     ).orNotFound
 
-    val appWithMiddleware =
-      Logger.httpApp(logHeaders = true, logBody = true)(httpApp)
+    val serviceWithLogging =
+      Logger.httpApp(logHeaders = true, logBody = true)(httpService)
+
+    val corsService = CORS(serviceWithLogging, corsConfig)
 
     BlazeServerBuilder[F](global)
       .bindHttp(port, "0.0.0.0")
-      .withHttpApp(appWithMiddleware)
+      .withHttpApp(corsService)
       .serve
   }
 
